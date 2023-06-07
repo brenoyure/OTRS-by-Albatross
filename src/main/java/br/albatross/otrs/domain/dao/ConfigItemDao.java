beee16c436e4305c94ed3ff205034ba423cb3945
Breno Yuri <byurifb@gmail.com>
2023-06-07 15:07:15 -0300

    New Constants e Order Desc
    A query ordena de maneira decrescente pelo Id do ConfigItemVersion, para que retorne o nº de série do registro mais recente de um computador.

--- src/main/java/br/albatross/otrs/domain/dao/ConfigItemDao.java
+++ src/main/java/br/albatross/otrs/domain/dao/ConfigItemDao.java
@@ -16,14 +16,19 @@ public class ConfigItemDao {
 	private EntityManager entityManager;
 	
 	/**
-	 * Nome da Versão do Item de configuração, nesse caso o BM.
+	 * No do atributo do XML que referencia a tabela/entidade ConfigItemVersion.
 	 */
-	private static final String CONFIG_ITEM_VERSION_NAME = "name";
+	private static final String CONFIG_ITEM_VERSION = "configItemVersion";
 	
 	/**
 	 * No do atributo do XML que referencia a tabela/entidade ConfigItemVersion.
 	 */
-	private static final String CONFIG_ITEM_VERSION = "configItemVersion";
+	private static final String ID = "id";
+
+	/**
+	 * Nome da Versão do Item de configuração, nesse caso o BM.
+	 */
+	private static final String CONFIG_ITEM_VERSION_NAME = "name";
 
 	/**
 	 * Atributo que representa um campo do XML.
@@ -34,14 +39,20 @@ public class ConfigItemDao {
 	 * Campo que será filtrado no XML, no caso o Número de Série.
 	 */
 	private static final String XML_CONTENT_KEY_NUMERO_DE_SERIE = "[1]{\'Version\'}[1]{\'NumeroDeSerie\'}[1]{\'Content\'}";
-	
+
+	/**
+	 * Busca o número de série pelo BM do computador utilizando a API de Criteria da JPA.
+	 * 
+	 * @param bm
+	 * @return optional contendo ou não o número de série.
+	 */
 	public Optional<String> findNumeroDeSerieByBmCriteria(String bm) {
 
 		try {
 			var cb          =  entityManager.getCriteriaBuilder();
 			var query       =  cb.createQuery(XmlStorage.class);
 			var xmlStorage  =  query.from(XmlStorage.class);
-
+			
 			xmlStorage.fetch(CONFIG_ITEM_VERSION, JoinType.INNER);
 
 			var predicateXmlKeyEqualsToConfigItemPkey = cb.equal(xmlStorage.get(CONFIG_ITEM_VERSION).get(CONFIG_ITEM_VERSION_NAME), bm);
@@ -50,7 +61,11 @@ public class ConfigItemDao {
 			var finalAndPredicate = cb.and(predicateXmlKeyEqualsToConfigItemPkey, predicateColumnEqualsToNumeroDeSerie);
 
 			query.where(finalAndPredicate);
-			
+
+			query.orderBy(
+						cb.desc(xmlStorage.get(CONFIG_ITEM_VERSION).get(ID)));
+
+
 			return Optional.of(entityManager.createQuery(query).setMaxResults(1).getSingleResult().getXmlContentValue());
 			
 		} catch (NoResultException e) {	return Optional.empty(); }
