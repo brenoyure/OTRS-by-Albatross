eb7fd92d0f7875e695b44aeddc6d7a059de7d992
Breno Yuri <byurifb@gmail.com>
2023-07-14 11:49:43 -0300

    Adicionados Comments xD
    Coloquei uma descrição nas interfaces de Solicitação de Garantia e E-mail de Garantia.

--- src/main/java/br/albatross/otrs/domain/models/garantia/apis/email/EmailDeGarantia.java
+++ src/main/java/br/albatross/otrs/domain/models/garantia/apis/email/EmailDeGarantia.java
@@ -8,31 +8,75 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+/**
+ * <p>Representa um E-mail de Garantia.</p>
+ * Esta interface extende a <code><strong>Serializable</strong></code> para que possa ser enviado, por exemplo, para uma fila de mensagens como a <code>jakarta.jms.Queue</code>.
+ */
 public interface EmailDeGarantia extends Serializable {
 
+	/**
+	 * 
+	 * @return O assunto do e-mail (<strong>Não</strong> podendo ficar em branco).
+	 */
 	@NotBlank
 	String getAssunto();
 
+	/**
+	 * 
+	 * @return O(s) anexo(s) do e-mail (<strong>Não</strong> podendo ficar nulo ou vazio).
+	 */
 	@NotEmpty
 	File[] getAnexos();
 
+	/**
+	 * 
+	 * @return O corpo da mensagem do e-mail (<strong>Não</strong> podendo ficar em branco).
+	 */
 	@NotBlank
 	String getCorpoDaMensagem();
 
+	/**
+	 * 
+	 * @return Os Dados do Envio (como remetente, destinatário...) do e-mail (<strong>Não</strong> pode ficar nulo).
+	 */
 	@NotNull
 	DadosDoEnvio getDadosDoEnvio();
 
+	/**
+	 * 
+	 * @return A Solicitação de Garantia a qual este e-mail pertence (<strong>Não</strong> podendo ficar nulo).
+	 */
 	@NotNull
 	SolicitacaoGarantia getSolicitacaoGarantia();
 
+	/**
+	 * 
+	 * Define assunto do e-mail (<strong>Não</strong> podendo ficar em branco).
+	 */
 	void setAssunto(String assunto);
 
+	/**
+	 * 
+	 * Define o(s) anexo(s) do e-mail (<strong>Não</strong> podendo ficar nulo ou vazio).
+	 */
 	void setAnexos(File[] anexos);
 
+	/**
+	 * 
+	 * Define o corpo da mensagem do e-mail (<strong>Não</strong> podendo ficar em branco).
+	 */
 	void setCorpoDaMensagem(String corpoDaMensagem);
 
+	/**
+	 * 
+	 * Define os Dados do Envio (como remetente, destinatário...) do e-mail (<strong>Não</strong> podendo ficar nulo).
+	 */
 	void setDadosDoEnvio(DadosDoEnvio dadosDoEnvio);
 
+	/**
+	 * 
+	 * Define a Solicitação de Garantia a qual este e-mail pertence. (<strong>Não</strong> podendo ficar nulo).
+	 */
 	void setSolicitacaoGarantia(SolicitacaoGarantia solicitacaoGarantia);
 
 }
--- src/main/java/br/albatross/otrs/domain/models/garantia/apis/solicitacao/SolicitacaoGarantia.java
+++ src/main/java/br/albatross/otrs/domain/models/garantia/apis/solicitacao/SolicitacaoGarantia.java
@@ -7,26 +7,61 @@ import br.albatross.otrs.domain.models.garantia.apis.email.EmailDeGarantia;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+/**
+ * Representa uma Solicitação de Garantia que pode ser aberta para algum fornecedor. 
+ */
 public interface SolicitacaoGarantia extends Serializable {
 
+	/**
+	 * 
+	 * @return O Nº de Série do Equipamento (ex. Computador, Monitor...) (<strong>Não</strong> podendo ficar em branco).
+	 */
 	@NotBlank
 	String getNumeroDeSerie();
 
+	/**
+	 * 
+	 * @return A Descrição do Problema (<strong>Não</strong> podendo ficar em branco).
+	 */
 	@NotBlank
 	String getDescricaoDoProblema();
 
+	/**
+	 * 
+	 * @return Os Dados do Chamado a qual esta solicitação pertence (<strong>Não</strong> podendo ficar nulo).
+	 */
 	@NotNull
 	DadosDoChamado getChamado();
 
+	/**
+	 * 
+	 * @return O E-mail que será enviado para o fabricante/fornecedor do equipamento (<strong>Não</strong> podendo ficar nulo).
+	 */
 	@NotNull
 	EmailDeGarantia getEmailDeGarantia();
 
+	/**
+	 * 
+	 * Define o Nº de Série do Equipamento (ex. Computador, Monitor...) (<strong>Não</strong> podendo ficar em branco).
+	 */
 	void setNumeroDeSerie(String numeroDeSerie);
 
+	/**
+	 * 
+	 * Define a Descrição do Problema (<strong>Não</strong> podendo ficar em branco).
+	 */
 	void setDescricaoDoProblema(String descricaoDoProblema);
 
+	/**
+	 * 
+	 * Define qual os Dados do Chamado a qual esta Solicitação pertence (<strong>Não</strong> podendo ficar em nulo).
+	 */
 	void setChamado(DadosDoChamado chamado);
 
+	/**
+	 * 
+	 * Define o E-mail que será enviado para o fabricante/fornecedor do equipamento (<strong>Não</strong> podendo ficar nulo).
+	 */
 	void setEmailDeGarantia(EmailDeGarantia emailDeGarantia);
 
 }
