c51133cac3bc373332a0cca3e1454588dbfd535b
Breno Yuri <byurifb@gmail.com>
2023-08-11 17:45:36 -0300

    GG
    Configuradas as variáveis de ambiente que permitem a rápida configuração do aplicativo em outros ambientes.

--- build-and-compose-up.sh
+++ build-and-compose-up.sh
@@ -1,9 +1,9 @@
 # Compila e enpacota o .war do projeto
-mvn clean package;
+ mvn clean package;
 
 # Copia o .war gerado para a pasta em que está localizado o dockerfile que será criado o app
-cp target/otrs.war ./docker/otrs-wildfly-docker/
+ cp target/otrs.war ./docker/otrs-wildfly-docker/
 
 # Navega até a  pasta que contém o compose.yaml e roda o compose up no projeto
 cd docker/ ;
-docker-compose up
+docker-compose up -d
