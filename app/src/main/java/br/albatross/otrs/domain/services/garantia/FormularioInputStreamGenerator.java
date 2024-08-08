062995dd62e9207b0f3d9cd45bff7230d305561b
Breno Yuri <byurifb@gmail.com>
2024-08-08 12:09:23 -0300

    Corrigido o gerador do formulario

--- app/src/main/java/br/albatross/otrs/domain/services/garantia/FormularioInputStreamGenerator.java
+++ app/src/main/java/br/albatross/otrs/domain/services/garantia/FormularioInputStreamGenerator.java
@@ -1,5 +1,7 @@
 package br.albatross.otrs.domain.services.garantia;
 
+import static java.io.File.separator;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,12 +13,12 @@ import jakarta.enterprise.context.RequestScoped;
 @RequestScoped
 public class FormularioInputStreamGenerator {
 
-//	private static final String ARQUIVOS_FOLDER = "/opt/jboss/wildfly/bin/arquivos";
-//	private static final String FORMULARIOS_FOLDER = "formularios";
+	private static final String ARQUIVOS_FOLDER = "/opt/jboss/wildfly/bin/arquivos";
+	private static final String FORMULARIOS_FOLDER = "formularios";
 	private static final String FORMULARIO_GARANTIA_DOCX = "Formulario-Garantia.docx";
 
 	public InputStream getInputStream() {
-		try { return new BufferedInputStream(new FileInputStream(new File(FORMULARIO_GARANTIA_DOCX))); } 
+	    try { return new BufferedInputStream(new FileInputStream(new File(ARQUIVOS_FOLDER + separator + FORMULARIOS_FOLDER, FORMULARIO_GARANTIA_DOCX))); }
 
 		catch (IOException e) { throw new RuntimeException(e); }
 
