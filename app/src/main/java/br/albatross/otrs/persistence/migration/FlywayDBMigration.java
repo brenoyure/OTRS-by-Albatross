b3325a74d1c89c2e590d9ab7155977d4c0bd0c46
Breno Yuri <byurifb@gmail.com>
2024-09-04 16:42:44 -0300

    xD

--- app/src/main/java/br/albatross/otrs/persistence/migration/FlywayDBMigration.java
+++ app/src/main/java/br/albatross/otrs/persistence/migration/FlywayDBMigration.java
@@ -24,6 +24,13 @@ public class FlywayDBMigration {
             .configure()
             .dataSource(dataSource)
             .locations("classpath:/flyway/db/migration")
+/*
+        O código comentado pode ser utilizado para implementar o Flyway em um ambiente em que já existe o schema criado.
+        Substitua o número da versão e o description de acordo com o último arquivo de migration.            
+            .baselineVersion("8")
+            .baselineDescription("Insert-into-Problema-and-DescricaoProblema")
+            .baselineOnMigrate(true)
+*/
             .load()
             .migrate();
 
