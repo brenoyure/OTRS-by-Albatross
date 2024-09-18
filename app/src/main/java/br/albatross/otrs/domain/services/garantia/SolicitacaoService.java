d05abb1d57a13fdd4e23a08c074a785e2e65f0fe
Breno Yuri <byurifb@gmail.com>
2024-09-18 14:59:27 -0300

    Melhorando a Performance na geração do JSON
    EJB Solicitação Service agora utilizar a API Jakarta JSON P para melhor performance na geração do JSON do Email de Garantia. (Ao custo de um código mais verboso)

--- app/src/main/java/br/albatross/otrs/domain/services/garantia/SolicitacaoService.java
+++ app/src/main/java/br/albatross/otrs/domain/services/garantia/SolicitacaoService.java
@@ -1,19 +1,28 @@
 package br.albatross.otrs.domain.services.garantia;
 
-import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 
+import br.albatross.apis.email.Email;
 import br.albatross.otrs.domain.models.garantia.apis.solicitacao.SolicitacaoDeGarantia;
+
 import jakarta.annotation.Resource;
+
 import jakarta.ejb.Asynchronous;
 import jakarta.ejb.Stateless;
+
 import jakarta.inject.Inject;
+
 import jakarta.jms.JMSConnectionFactory;
 import jakarta.jms.JMSContext;
 import jakarta.jms.Queue;
-import jakarta.jms.TextMessage;
-import jakarta.json.bind.Jsonb;
-import jakarta.json.bind.JsonbBuilder;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.json.stream.JsonGenerator;
+
 import jakarta.validation.Valid;
 
 /**
@@ -38,19 +47,62 @@ public class SolicitacaoService {
     @Asynchronous
 	public void solicitarGarantia(@Valid SolicitacaoDeGarantia solicitacaoDeGarantia) {
 
-	    Jsonb jsonb = JsonbBuilder.create();
-	    StringWriter stringWriter = new StringWriter();
-		BufferedWriter bufferedWriter = new BufferedWriter(stringWriter);
-	    jsonb.toJson(solicitacaoDeGarantia.getEmailDeGarantia(), bufferedWriter);
+        try (StringWriter stringWriter = new StringWriter()) {
+            try (JsonGenerator jsonGenerator = Json.createGenerator(stringWriter)) {
 
-        TextMessage textMessage = context.createTextMessage(stringWriter.toString());
-        context.createProducer().send(queue, textMessage);
+                Email email = 
+                        solicitacaoDeGarantia.getEmailDeGarantia();                    
 
-	    try {
-			jsonb.close();
-			bufferedWriter.close();
-			stringWriter.close();
-	    } catch (Exception e) { throw new RuntimeException(e); }
+                jsonGenerator
+                    .writeStartObject()
+
+                    .write("assunto",         email.getAssunto())
+                    .write("corpoDaMensagem", email.getCorpoDaMensagem())
+                    .write("remetente",       email.getRemetente())
+                    .write("destinatario",    email.getDestinatario());
+
+                if (email.getCopiaPara() != null && !email.getCopiaPara().isBlank()) {
+                    jsonGenerator.write("copiaPara", email.getCopiaPara());
+                }
+
+                if ( email.getAnexos() != null && !email.getAnexos().isEmpty() ) {
+
+                    jsonGenerator.writeStartArray("anexos");
+
+                    email.getAnexos().forEach(anexo -> {
+
+                        jsonGenerator
+                            .writeStartObject()
+                            .write("nome", anexo.getNome())
+                            .writeStartArray("arquivo");
+
+                        for (byte b : anexo.getArquivo()) {
+                            jsonGenerator.write(b);
+                        }
+
+                        jsonGenerator
+                            .writeEnd()
+                            .writeEnd();
+
+                    });
+
+                    jsonGenerator.writeEnd();
+
+                }
+
+                jsonGenerator.writeEnd();
+
+            }
+
+            String emailDeGarantiaAsJson = stringWriter.toString();
+
+            JsonReader jsonReader = Json.createReader(new StringReader(emailDeGarantiaAsJson));
+            JsonObject jsonObject = jsonReader.readObject();
+            System.out.println(jsonObject.toString());
+
+            context.createProducer().send(queue, emailDeGarantiaAsJson);
+
+        } catch (IOException e) { throw new RuntimeException(e); }
 
 	}
 
